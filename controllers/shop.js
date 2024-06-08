316edc2a38a9f1f12ff3caeb20a78289344d3cbc
nayan1908 <nayanpatel807@gmail.com>
2024-06-08 16:11:03 +0530

    add cart data to orders and cleart cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product');
+const Order = require('../models/Order');
+const { populate } = require('../models/user');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -47,7 +49,7 @@ exports.getCart = (req, res, next) => {
     .populate('cart.items.productId')
     .then(user => {
       const products = user.cart.items;
-      
+
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
@@ -112,16 +114,32 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
-    .then(result => {
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+
+      return order.save();
+    }).then(() => {
+      return req.user.clearCart();
+    }).then(() => {
+
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
