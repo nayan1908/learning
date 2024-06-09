4f98a0c4d85da83c86e04c4ac2f3fcb16096d3c2
nayan1908 <nayanpatel807@gmail.com>
2024-06-09 18:50:43 +0530

    1. set sessin after login and store data in mongodb collection 2. delete session after logout 3. set cookie in browser

--- app.js
+++ app.js
@@ -4,26 +4,44 @@ const { PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE } = require('./util/con
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
-const errorController = require('./controllers/error');
+const session = require("express-session");
+const MongodbStore = require("connect-mongodb-session")(session);
 
-// const Product = require('./models/product');
+const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@cluster0.gfcuwq7.mongodb.net/${DATABASE}?retryWrites=true&w=majority&appName=Cluster0`;
+
 const app = express();
+const store = new MongodbStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('66612c6630f98916224d5704')
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
   .then(user => {
     req.user = user;
     next();
@@ -32,10 +50,11 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(`mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@cluster0.gfcuwq7.mongodb.net/${DATABASE}?retryWrites=true&w=majority&appName=Cluster0`)
+mongoose.connect(MONGODB_URI)
   .then((result) => {
 
     User.findOne().then(user => {
---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+    // const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    User.findById('66612c6630f98916224d5704')
+        .then(user => {
+            req.session.user = user;
+            req.session.isLoggedIn = true;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect("/");
+            })
+            // res.setHeader('Set-Cookie', 'loggedIn=true');
+        }).catch(err => console.log(err));
+
+}
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        res.redirect("/");
+    });
+}
