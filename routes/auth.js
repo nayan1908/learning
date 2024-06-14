535adb9f49977f98f61e01d2a34fc027b6d8c0e7
nayan1908 <nayanpatel807@gmail.com>
2024-06-14 22:36:04 +0530

    signup form created

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,10 @@ const authController = require("../controllers/auth");
 
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
-router.post('/logout', authController.postLogout);
+
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postLogin);
+
+router.post('/logout', authController.postSignup);
 
 module.exports = router;
