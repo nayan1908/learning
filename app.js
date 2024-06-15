deef4d1605b1bda0dc85b112d91a551db56b901a
nayan1908 <nayanpatel807@gmail.com>
2024-06-15 13:02:58 +0530

    csurf package install & csrf token added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongodbStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -17,6 +18,8 @@ const store = new MongodbStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -37,6 +40,8 @@ app.use(
   })
 );
 
+app.use(csrfProtection);
+
 app.use((req, res, next) => {
   if(!req.session.user){
     return next();
@@ -47,6 +52,12 @@ app.use((req, res, next) => {
   next();
   }).catch(err => console.log(err));
 })
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isAuthenticated;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
